87113c238c50b884c8b9057e1120cd78c7faa061
MCPCapital <matthewpilsbury@protonmail.com>
2020-08-04 19:09:24 +0100

    New Setup Installer!

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ fi
 apt update --yes
 apt dist-upgrade --yes
 apt upgrade --yes
-apt install --yes git python3 pip3 python3-dev libpython-dev python3-opencv libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
-pip3 install -y http_parser
+apt install --yes git python3 python3-pip python3-dev libpython-dev python3-opencv libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
+pip3 install  http_parser
 git clone https://github.com/Synss/python-mbedtls
 cd python-mbedtls
-pip3 install -r -y requirements.txt
+pip3 install -r requirements.txt
 python3 ./setup.py install
 exit N
