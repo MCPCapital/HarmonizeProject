0be4b0335740c3d71e8f6ef2fe90497841467347
MCP Capital, LLC <matthewpilsbury@protonmail.com>
2020-08-04 14:00:06 -0400

    Create setup script - DO NOT USE YET

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root / with sudo" 
+   exit 1
+fi
+apt update
+apt dist-upgrade
+apt upgrade
+apt install --yes git python3 pip3 python3-dev libpython-dev python3-opencv libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
+pip3 install http_parser
+git clone https://github.com/Synss/python-mbedtls
+cd python-mbedtls
+pip3 install -r requirements.txt
+python3 ./setup.py install
+exit N
