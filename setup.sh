1cefbb9aa1cececc402f8fae53a561a19c4220d5
MCP Capital, LLC <matthewpilsbury@protonmail.com>
2020-08-12 14:44:31 -0400

    Add http-parser

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ fi
 apt update --yes
 apt dist-upgrade --yes
 apt upgrade --yes
-apt install --yes git python3 python3-pip python3-requests python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
+apt install --yes git python3 python3-pip python3-requests python3-http-parser python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
 echo "############### BASE DEPENDENCIES DONE ###############" 
 apt --yes install python3-opencv
 echo "############### OPENCV DONE ###############" 
