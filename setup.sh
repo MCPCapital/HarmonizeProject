3a76d4d6616b79915c61164ffd6979d9e2303b64
MCP Capital, LLC <matthewpilsbury@protonmail.com>
2020-08-12 09:37:52 -0400

    Update setup.sh
    Remove libjpeg as turbo version works fine & is a conflict

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ fi
 apt update --yes
 apt dist-upgrade --yes
 apt upgrade --yes
-apt install --yes git python3 python3-pip python3-dev libpython-dev python3-opencv libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
+apt install --yes git python3 python3-pip python3-dev libpython-dev python3-opencv libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
 pip3 install  http_parser
 git clone https://github.com/Synss/python-mbedtls
 cd python-mbedtls
