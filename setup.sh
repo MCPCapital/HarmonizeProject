136673fa4c4283605b76eabe5cfe72e2f1f8c828
Matthew C. Pilsbury <matt@mcpcapital.net>
2022-08-23 12:17:53 -0400

    Cleanup setup script
    'apt dist-upgrade' may perform more upgrades than the user intends, and is not necessary for proper functionality. Also minor text changes.
    Glad to see people are still using this :)

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root / with sudo!" 
+   echo "This script must be run as root or with sudo!" 
    exit 1
 fi
-apt update --yes
-apt dist-upgrade --yes
-apt upgrade --yes
-apt install --yes git python3 python3-pip python3-requests python3-http-parser python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
+apt update -y
+apt upgrade -y
+apt install -y git python3 python3-pip python3-requests python3-http-parser python3-dev libpython-dev libqtgui4 libqt4-test libgstreamer1.0-0 libjpeg62-turbo-dev libmbedtls12 libmbedtls-dev screen
 echo "############### BASE DEPENDENCIES DONE - CHECK FOR ERRORS ###############" 
-apt --yes install python3-opencv
+apt -y install python3-opencv
 echo "############### OPENCV DONE - CHECK FOR ERRORS ###############" 
-echo "############### FULLY COMPLETE - CHECK FOR ERRORS ###############" 
+echo "############### FULLY COMPLETE ###############" 
 exit
